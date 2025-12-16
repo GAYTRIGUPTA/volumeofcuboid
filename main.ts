2c2f2c29881ec1e62475f4984bf849820f79c561
GAYTRIGUPTA <127135239+GAYTRIGUPTA@users.noreply.github.com>
2025-12-16 20:24:54 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+function VolumeOfCuboid (L: string, B: string, H: string) {
+    let height = 0
+    let breadth = 0
+    let length = 0
+    volume = length * (breadth * height)
+    player.say("Volume of Cuboid is")
+    player.say("volume")
+}
+let volume = 0
+volume = 0
+VolumeOfCuboid("5", "6", "10")
